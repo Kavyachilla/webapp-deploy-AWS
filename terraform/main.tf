55f776704452f8655c7ae1ca520def52f4f63bc4
Kavyachilla <kavyachilla30@gmail.com>
2025-07-25 17:15:11 +0530

    3rd run

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,10 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_dns_hostnames   = true
-  enable_dns_support     = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 # EKS Module
@@ -26,4 +26,19 @@ module "eks" {
   version = "20.8.4"
 
   cluster_name    = "webapp-cluster"
-  cluster_version = "1._
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    web_nodes = {
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  enable_irsa = true
+}
